d2360804a80fa418e4f1223e09090e7055f99005
Ibra <70988546+ibra-dotcom@users.noreply.github.com>
2024-03-19 22:38:06 -0400

    Import libraries

---
+++ Model.py
@@ -0,0 +1,5 @@
+#Import libraries: pandas and scikit-learn methods
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import ensemble
+from sklearn.metrics import mean_absolute_error
