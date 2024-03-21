5deb41cc7f2b821ff11307ea67e5662c8d26de1d
Ibra <70988546+ibra-dotcom@users.noreply.github.com>
2024-03-20 22:34:26 -0400

    Update Model.py
    Scrub the data

--- Model.py
+++ Model.py
@@ -5,4 +5,21 @@ from sklearn import ensemble
 from sklearn.metrics import mean_absolute_error
 
 #Import dataset
-df = pd.read_csv('~/Downloads/Melbourne_housing_FULL.csv')
+df = pd.read_csv('~/ML/Machine Learning/Melbourne_housing_FULL.csv')
+
+#Scrub dataset
+# The misspellings of “longitude” and “latitude” are preserved here del df['Address']
+del df['Method']
+del df['SellerG']
+del df['Date']
+del df['Postcode']
+del df['Lattitude']
+del df['Longtitude'] 
+del df['Regionname'] 
+del df['Propertycount']
+df.dropna(axis = 0, how = 'any', thresh = None, subset = None, inplace = True)
+df = pd.get_dummies(df, columns = ['Suburb', 'CouncilArea', 'Type']
+X = df.drop('Price',axis=1) y = df['Price']
+
+#Split the Dataset
+
