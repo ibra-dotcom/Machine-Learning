72de45fc25746f53847e5dc93cd91f1e9f7e75aa
Ibra <70988546+ibra-dotcom@users.noreply.github.com>
2024-03-20 22:25:18 -0400

    Update Model.py

--- Model.py
+++ Model.py
@@ -3,3 +3,5 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import ensemble
 from sklearn.metrics import mean_absolute_error
+
+#Import df = pd.read_csv('~/Downloads/Melbourne_housing_FULL.csv')
